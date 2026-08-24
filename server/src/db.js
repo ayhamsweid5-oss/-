7bed6646ffea5a59955d9330ebbef2c8933d4881
ayhamsweid5-oss <ayhamsweid5-oss@users.noreply.github.com>
2026-08-24 18:16:36 +0300

    Support Netlify Database connection string

--- server/src/db.js
+++ server/src/db.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: process.env.NETLIFY_DB_URL || process.env.DATABASE_URL });
 module.exports = { pool, query: (text, params) => pool.query(text, params) };
+
